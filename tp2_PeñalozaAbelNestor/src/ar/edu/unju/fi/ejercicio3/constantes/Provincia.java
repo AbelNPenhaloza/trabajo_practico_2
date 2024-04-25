2f19fbb2915106643d7baf3383fc5f1cb7f54ffd
Abel Nestor Peñaloza <abelnrpenaloza@gmail.com>
2024-04-25 12:48:45 -0300

    Se programo el Enum con lo que pide el ejercicio 3
    Descripcion: Se declararon los atributos el constructor, metodos accesores y metodo para calcular la desnsidad poblacional.
    Autor: Abel N Penhaloza

--- tp2_PeñalozaAbelNestor/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
+++ tp2_PeñalozaAbelNestor/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
@@ -1,5 +1,45 @@
 package ar.edu.unju.fi.ejercicio3.constantes;
 
 public enum Provincia {
+	JUJUY,
+	SALTA,
+	TUCUMAN,
+	CATAMARCA,
+	LA_RIOJA,
+	SANTIAGO_DEL_ESTERO;
+	
+	private Integer cantidadPoblacion;
+	private Double superficie;
+	/**
+	 * @param cantidadPoblacion
+	 * @param superficie
+	 */
+	private Provincia() {
+		this.cantidadPoblacion = null;
+		this.superficie = null;
+	}
+	//Metodo para calcular la densidad Poblacional de una Provincia.
+	public Double calcularDensidadPoblacional() {
+		if(cantidadPoblacion != null && superficie != null && superficie !=0) {
+			return cantidadPoblacion / superficie;
+		}else {
+			return 0.0;
+		}
+	}
+	//Metodos accesores
+	
+	public Integer getCantidadPoblacion() {
+		return cantidadPoblacion;
+	}
+	public void setCantidadPoblacion(Integer cantidadPoblacion) {
+		this.cantidadPoblacion = cantidadPoblacion;
+	}
+	public Double getSuperficie() {
+		return superficie;
+	}
+	public void setSuperficie(Double superficie) {
+		this.superficie = superficie;
+	}
 
+	
 }
